2cc17baad0d7af63789ec5d6aaa4b5bffe49a6bf
Hans Elias B. Josephsen <me@hansihe.com>
2021-06-23 13:19:01 +0200

    update edit urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,13 +85,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/rusterlium/web/edit/master/website/',
+            'https://github.com/rusterlium/web/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/rusterlium/web/edit/master/website/blog/',
+            'https://github.com/rusterlium/web/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
